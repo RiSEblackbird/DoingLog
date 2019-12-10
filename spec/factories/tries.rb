6d94ce60860782ece10fd9fc8650ac9b59b5b6fb
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-10 15:49:45 +0900

    Setting of factory_bot models (doing_log, problem, try, user)

--- spec/factories/tries.rb
+++ spec/factories/tries.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :try do
-    
+    title: Faker::Base.regexify("[tryふぃ56]{100}")
+    summary: Faker::Base.regexify("[tryぃ57さあ]{1000}")
+    effective: true
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username: Faker::Base.regexify("[userあ5]{30}")
+    profile: Faker::Base.regexify("[userあ123gkj]{150}")
+    email: Faker::Internet.email
+    password: Faker::Lorem.characters(number: 20)
   end
 end
