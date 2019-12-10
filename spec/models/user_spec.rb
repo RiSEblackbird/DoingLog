b8c8528e5b099f0eabe2cf364cf4a18543421b66
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-10 20:05:50 +0900

    Fix user_spec using FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,90 +2,49 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "成功例：username, Email, Password" do
-    user = User.new(
-      username: Faker::Base.regexify("[aあ]{30}"),
-      profile: Faker::Base.regexify("[aあかさたな123gkrogj]{150}"),
-      email: Faker::Internet.email,
-      password: Faker::Lorem.characters(number: 20),
-    )
+    user = FactoryBot.build(:user)
     expect(user).to be_valid
   end
 
   it "文字制限(上限)：usernameを31文字で登録する" do
-    user = User.new(
-      username: Faker::Base.regexify("[bごほ]{31}"),
-      profile: Faker::Base.regexify("[aあかさたな123gkrogj]{151}"),
-      email: "mail@mail.com",
-      password: "s6bdtr7vfb",
-    )
+    user = FactoryBot.build(:user, username: Faker::Base.regexify("[bごほ]{31}"))
+    user.valid?
     expect(user.errors[:username]).to include("ユーザー名は30文字以内にしてください。")
   end
 
   it "文字制限(上限)：profileを151文字で登録する" do
-    user = User.new(
-      username: Faker::Base.regexify("[bごほ]{30}"),
-      profile: Faker::Base.regexify("[aあかさたな123gkrogj]{151}"),
-      email: "mail@mail.com",
-      password: "s6bdtr7vfb",
-    )
+    user = FactoryBot.build(:user, profile: Faker::Base.regexify("[aあかさたな123gkrogj]{151}"))
+    user.valid?
     expect(user.errors[:username]).to include("プロフィール文は150文字以内にしてください。")
   end
 
   it "文字制限(上限)：passwordを21文字で登録する" do
-    user = User.new(
-      username: Faker::Base.regexify("[bごほ]{30}"),
-      profile: Faker::Base.regexify("[aあかさgsdfな123gkrogj]{150}"),
-      email: "mail@mail.com",
-      password: Faker::Lorem.characters(number: 21),
-    )
+    user = FactoryBot.build(:user, password: Faker::Lorem.characters(number: 21))
+    user.valid?
     expect(user.errors[:username]).to include("パスワードは20文字以内にしてください。")
   end
 
   it "重複不可：既存ユーザーで使用されているemailを登録する" do
-    User.create(
-      username: "KONO",
-      profile: "国王",
-      email: "TARO@mail.com",
-      password: "5fdsmh7ag",
-    )
-    user = User.new(
-      username: "ASO",
-      profile: "国王",
-      email: "TARO@mail.com",
-      password: "f54fasdas6",
-    )
+    FactoryBot.create(:user, email: "AAA@mail.com")
+    user = FactoryBot.build(:user, email: "AAA@mail.com")
+    user.valid?
     expect(user.errors[:email]).to include("Eメールアドレスはすでに使用されています。")
   end
 
   it "必須入力：usernameの未入力" do
-    user = User.new(
-      username: nil,
-      profile: "関白",
-      email: "AAA@mail.com",
-      password: "gd7a6dfvh",
-    )
+    user = FactoryBot.build(:user, username: nil)
     user.valid?
     expect(user.errors[:username]).to include("ユーザー名は必須入力項目です。")
   end
 
   it "必須入力：emailの未入力" do
-    user = User.new(
-      username: "魚雷",
-      profile: "関白",
-      email: nil,
-      password: "gd7a6dfvh",
-    )
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("Eメールは必須入力項目です。")
   end
 
   it "必須入力：passwordの未入力" do
-    user = User.new(
-      username: "魚雷",
-      profile: "関白",
-      email: "AAA@gmail.com",
-      password: nil,
-    )
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("パスワードを入力してください。")
   end
