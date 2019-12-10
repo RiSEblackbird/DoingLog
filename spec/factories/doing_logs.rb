b404fadeaf35c6ef5968aa41ca933f496f2b567b
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-10 19:21:03 +0900

    Fix typo : factories/<model>.rb

--- spec/factories/doing_logs.rb
+++ spec/factories/doing_logs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :doing_log do
-    title: Faker::Base.regexify("[doinglogふぃ56]{100}")
-    summary: Faker::Base.regexify("[doinglogぃ57おさ]{1000}")
+    title Faker::Base.regexify("[doinglogふぃ56]{100}")
+    summary Faker::Base.regexify("[doinglogぃ57おさ]{1000}")
   end
 end
--- spec/factories/problems.rb
+++ spec/factories/problems.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :problem do
-    title: Faker::Base.regexify("[problemふぃ56]{100}")
-    summary: Faker::Base.regexify("[problemぃ57さあ]{1000}")
-    solved: true
+    title Faker::Base.regexify("[problemふぃ56]{100}")
+    summary Faker::Base.regexify("[problemぃ57さあ]{1000}")
+    solved true
   end
 end
