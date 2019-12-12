51d1cbeac25441dbb21c2f0014cbe1d1bbd78afa
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-12 12:27:22 +0900

    Fix try_spec using FactoryBot

--- spec/models/try_spec.rb
+++ spec/models/try_spec.rb
@@ -2,45 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Try, type: :model do
   it "成功例：title, body" do
-    try = Try.new(
-      title: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{100}"),
-      body: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{1000}"),
-    )
+    try = FactoryBot.build(:problem)
+    expect(try).to be_valid
   end
 
   it "文字制限(上限)：titleを101文字で登録する" do
-    try = Try.new(
-      title: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{101}"),
-      body: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{1000}"),
-    )
+    try = FactoryBot.build(:try, title: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{101}"))
+    try.valid?
     expect(try.errors[:title]).to include("タイトルは100文字以内にしてください。")
   end
 
   it "文字制限(上限)：bodyを1001文字で登録する" do
-    try = Try.new(
-      title: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{100}"),
-      body: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{1001}"),
-    )
+    try = FactoryBot.build(:try, title: Faker::Base.regexify("[aふぃ5786おさgjgsgあ]{1001}"))
+    try.valid?
     expect(try.errors[:body]).to include("概要は1000文字以内にしてください。")
   end
 
   it "重複不可：既存タイトル(title)を登録する" do
-    Try.create(
-      title: "gem ○○○の導入",
-      body: "退勤ラッシュの京王井の頭線の外側方面で、停車時に扉が開くと車外に弾き飛ばされてしまう人を受けるネットの設置。",
-    )
-    try = Try.new(
-      title: "gem ○○○の導入",
-      body: "新しい昼寝のあり方をプロデュースするも全くウケないので聴衆にサクラを仕込む。",
-    )
+    FactoryBot.create(:try, title: "○○の適用.")
+    try = FactoryBot.build(:try, title: "○○の適用.")
+    try.valid?
     expect(try.errors[:title]).to include("既に使用されているタイトルは登録できません。")
   end
 
   it "必須入力：titleの未入力" do
-    try = Try.new(
-      title: nil,
-      body: "退勤ラッシュの京王井の頭線の外側方面で、停車時に扉が開くと数人が車外に弾き飛ばされてしまう。",
-    )
+    try = FactoryBot.build(:try, title: nil))
     try.valid?
     expect(try.errors[:title]).to include("取り組み事項のタイトルは必須入力項目です。")
   end
