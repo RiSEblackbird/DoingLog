02deaadcca0024af569d2662bfd447caf1e409ce
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-12 15:04:15 +0900

    rails_helper : Apply Devise helper to RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  # capybaraを適用
   require 'capybara/rspec'
+
+  # Devise提供のログイン検証用のヘルパーを適用
+  config.include
+  Devise::Test::ControllerHelpers, type: :controller
+
 end
