f9739db18ac0d86b270951021c4d4602e8a3740f
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-15 15:57:10 +0900

    $ bundle exec rails g rspec:feature doing_log_new

--- spec/features/doing_log_new_spec.rb
+++ spec/features/doing_log_new_spec.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.feature 'NewDoings', type: :feature do
+RSpec.feature "DoingLogNews", type: :feature do
   pending "add some scenarios (or delete) #{__FILE__}"
 end
