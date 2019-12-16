d3920f12b13f8e2aea94c8238733851b07cfe8e0
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-16 23:14:00 +0900

    factories/tries : fix wrong column name

--- spec/factories/tries.rb
+++ spec/factories/tries.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :try do
     title Faker::Base.regexify('[tryふぃ56]{100}')
-    summary Faker::Base.regexify('[tryぃ57さあ]{1000}')
+    body Faker::Base.regexify('[tryぃ57さあ]{1000}')
     effective true
     association :user
     association :problem
