03af9118f6dee405f9c3ef708f4cac3d607f33a6
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-16 14:52:22 +0900

    doing_log_edit_spec : divide 'user login' and 'doing_log edit' process

--- spec/features/doing_log_edit_spec.rb
+++ spec/features/doing_log_edit_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "DoingLogEdits", type: :feature do
-  scenario "DoingLogの編集完了まで" do
+  scenario "ある特定のDoingLogの編集完了まで" do
     user = FactoryBot.create(:user)
     doing_log = FactoryBot.create(:doing_log, title: "Swaggerの使用方法の学習",
       summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
@@ -12,10 +12,10 @@ RSpec.feature "DoingLogEdits", type: :feature do
     fill_in "Eメール", with: user.email
     fill_in "password", with: user.password
     click_button "ログイン"
-    click_link "/doing_logs/#{doing_log.id}"
-    click_link "編集"
 
     expect {
+      click_link "/doing_logs/#{doing_log.id}"
+      click_link "編集"
       click_link "Swaggerの使用方法の学習"
       fill_in "取り組みタイトル", with: "目玉焼きの改良"
       fill_in "概要", with: "鶏卵の調理行為が課税対象となったため、代替材料の模索と、適合する新たな調理方法を検討する。"
