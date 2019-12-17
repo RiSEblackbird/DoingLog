b517aa2029858c54b0e9159588113dba632a9119
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-17 11:20:24 +0900

    doing_log_index_spec : access to #index

--- spec/features/doing_log_index_spec.rb
+++ spec/features/doing_log_index_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "DoingLogIndices", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  user = FactoryBot.create(:user)
+    doing_log = FactoryBot.create(:doing_log, title: "Swaggerの使用方法の学習",
+      summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
+
+    sign_in_as user
+
+    expect {
+      click_link "/doing_logs"
+
+      expect(page).to have_content "取り組み事項一覧"
+      expect(page).to have_content "Swaggerの使用方法の学習"
+      expect(page).to have_content "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。"
+    }.to be_success
 end
