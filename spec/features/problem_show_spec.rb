e0a5c927a566ad1045c4d9cf4dffe01456d184e0
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-17 17:02:53 +0900

    problem_show_spec : new problem scenario

--- spec/features/problem_show_spec.rb
+++ spec/features/problem_show_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "ProblemShows", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "DoingLog詳細にてProblemの新規追加完了まで" do
+    user = FactoryBot.create(:user)
+    doing_log = FactoryBot.create(:doing_log, title: "Swaggerの使用方法の学習",
+      summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
+    problem = FactoryBot.create(:problem, title: "日中の室内照度不足",
+      summary: "部屋がビルの陰に位置するため、自然光による照度が確保できない。", user: user, doing_log: doing_log)
+
+    sign_in_as user
+
+    expect {
+      click_link "/problems/#{problem.id}"
+
+      expect(page).to have_content "日中の室内照度不足"
+      expect(page).to have_content "部屋がビルの陰に位置するため、自然光による照度が確保できない。"
+    }.to be_success
+  end
 end
