df36c5c9a14a91ea0de59d6f8ce23131720727d3
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-17 11:21:39 +0900

    problem_delete_spec : delete a problem

--- spec/features/problem_delete_spec.rb
+++ spec/features/problem_delete_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "ProblemDeletes", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "ある特定のDoingLogの削除完了まで" do
+    user = FactoryBot.create(:user)
+    problem = FactoryBot.create(:problem, title: "×実装時の○○エラー",
+      summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
+
+    sign_in_as user
+
+    expect {
+      click_link "/problems/#{problem.id}"
+      click_link "削除"
+      page.accept_alert
+      expect(page).to have_content "削除しました。"
+    }.to change(user.problems, :count).by(-1)
+  end
 end
