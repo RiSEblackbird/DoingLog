2623bedc9027c1db2b8b90c86cc6d8fcb01ca5e5
SELA <RiSEblackbird@users.noreply.github.com>
2019-12-17 16:31:32 +0900

    doing_log_index_spec : fix indent, scenario name

--- spec/features/doing_log_index_spec.rb
+++ spec/features/doing_log_index_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "DoingLogIndices", type: :feature do
-  user = FactoryBot.create(:user)
-  another_user = FactoryBot.create(:user)
-  doing_log = FactoryBot.create(:doing_log, title: "Swaggerの使用方法の学習",
-    summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
-  another_doing_log = FactoryBot.create(:doing_log, title: "Postmanの使用方法の学習",
-    summary: "API開発でよく使用されるらしいPostmanの使用方法を探る。", user: another_user)
 
-  sign_in_as user
+  scenario "DoingLogの一覧ページの表示まで" do
+    user = FactoryBot.create(:user)
+    another_user = FactoryBot.create(:user)
+    doing_log = FactoryBot.create(:doing_log, title: "Swaggerの使用方法の学習",
+      summary: "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。", user: user)
+    another_doing_log = FactoryBot.create(:doing_log, title: "Postmanの使用方法の学習",
+      summary: "API開発でよく使用されるらしいPostmanの使用方法を探る。", user: another_user)
 
-  expect {
-    click_link "/doing_logs"
+    sign_in_as user
 
-    expect(page).to have_content "取り組み事項一覧"
-    expect(page).to have_content "Swaggerの使用方法の学習"
-    expect(page).to have_content "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。"
-    expect(page).to have_content "Postmanの使用方法の学習"
-    expect(page).to have_content "API開発でよく使用されるらしいPostmanの使用方法を探る。"
-  }.to be_success
+    expect {
+      click_link "/doing_logs"
+
+      expect(page).to have_content "取り組み事項一覧"
+      expect(page).to have_content "Swaggerの使用方法の学習"
+      expect(page).to have_content "API開発でよく使用されるらしいSwagger Editorの使用方法を探る。"
+      expect(page).to have_content "Postmanの使用方法の学習"
+      expect(page).to have_content "API開発でよく使用されるらしいPostmanの使用方法を探る。"
+    }.to be_success
+  end
 end
