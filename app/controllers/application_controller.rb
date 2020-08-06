19412d40d26aedd15cabe53f77a60942328f822b
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-06 13:47:46 +0900

    サインアップのパラメータを追加 https://github.com/heartcombo/devise#strong-parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # サインアップのパラメータを追加する https://github.com/heartcombo/devise#strong-parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
