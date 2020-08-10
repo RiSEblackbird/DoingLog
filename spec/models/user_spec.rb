63a4926e31474f3482d7c75bd1885774973a7f54
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-10 19:33:59 +0900

    スペックのメッセージから変数名を削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,43 +11,43 @@ RSpec.describe User, type: :model do
   it '文字制限(上限)：usernameを31文字で登録する' do
     user = FactoryBot.build(:user, username: Faker::Base.regexify('[aあア亜ｱ123gkａー-]{31}'))
     user.valid?
-    expect(user.errors[:username]).to include('ユーザー名は30文字以内で入力してください')
+    expect(user.errors[:username]).to include('は30文字以内で入力してください')
   end
 
   it '文字制限(上限)：profileを151文字で登録する' do
     user = FactoryBot.build(:user, profile: Faker::Base.regexify('[aあア亜ｱ123gkａー-]{151}'))
     user.valid?
-    expect(user.errors[:username]).to include('プロフィール文は150文字以内で入力してください')
+    expect(user.errors[:username]).to include('は150文字以内で入力してください')
   end
 
   it '文字制限(上限)：passwordを21文字で登録する' do
     user = FactoryBot.build(:user, password: Faker::Lorem.characters(number: 21))
     user.valid?
-    expect(user.errors[:username]).to include('パスワードは20文字以内で入力してください')
+    expect(user.errors[:username]).to include('は20文字以内で入力してください')
   end
 
   it '重複不可：既存ユーザーで使用されているemailを登録する' do
     FactoryBot.create(:user, email: 'AAA@mail.com')
     user = FactoryBot.build(:user, email: 'AAA@mail.com')
     user.valid?
-    expect(user.errors[:email]).to include('Eメールはすでに存在します')
+    expect(user.errors[:email]).to include('はすでに存在します')
   end
 
   it '必須入力：usernameの未入力' do
     user = FactoryBot.build(:user, username: nil)
     user.valid?
-    expect(user.errors[:username]).to include('ユーザー名を入力してください')
+    expect(user.errors[:username]).to include('を入力してください')
   end
 
   it '必須入力：emailの未入力' do
     user = FactoryBot.build(:user, email: nil)
     user.valid?
-    expect(user.errors[:email]).to include('Eメールを入力してください')
+    expect(user.errors[:email]).to include('を入力してください')
   end
 
   it '必須入力：passwordの未入力' do
     user = FactoryBot.build(:user, password: nil)
     user.valid?
-    expect(user.errors[:password]).to include('パスワードを入力してください')
+    expect(user.errors[:password]).to include('を入力してください')
   end
 end
