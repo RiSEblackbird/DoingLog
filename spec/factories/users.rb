4cc163a41b54a25aa580f4c429f8267a11da570a
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-10 23:25:06 +0900

    spec/factories/users.rb : 'id'を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :user do
+    id { Faker::Lorem.characters(number: 4) }
     username { Faker::Base.regexify('[userあ5]{30}') }
     profile { Faker::Base.regexify('[userあ123gkj]{150}') }
     email { Faker::Internet.email }
