e026fb33fc7740cbe0689ad637ae455aed77af7d
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-12 01:04:09 +0900

    バリデーションのエラーメッセージを表示
    3.3 :message - Railsガイド
    https://railsguides.jp/active_record_validations.html#message

--- app/models/doing.rb
+++ app/models/doing.rb
@@ -3,7 +3,7 @@
 class Doing < ApplicationRecord
   belongs_to :user, counter_cache: :doing_count
 
-  validates :title, presence: true, message: "取り組み事項のタイトルは必須入力項目です"
-  validates :title, length: { maximum: 100, message: "タイトルは%{length}文字以内にしてください" }
-  validates :summary, length: { maximum: 1000, message: "概要は%{length}文字以内にしてください" }
+  validates :title, presence: { message: "取り組み事項のタイトルは必須入力項目です" }
+  validates :title, length: { maximum: 100, message: "タイトルは100文字以内にしてください" }
+  validates :summary, length: { maximum: 1000, message: "概要は1000文字以内にしてください" }
 end
