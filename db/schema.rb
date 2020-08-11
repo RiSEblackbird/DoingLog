504f3c14b506406275df508d1c86ae28d4e96613
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-11 18:46:09 +0900

    AddDoingCountToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_051348) do
+ActiveRecord::Schema.define(version: 2020_08_11_094400) do
 
   create_table "doings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_051348) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin_flg", default: false
     t.boolean "guest_flg", default: false
+    t.integer "doing_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
