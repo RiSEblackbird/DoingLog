6ac62e73d4774823cbb0a81645fd6453c13abc47
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-12 22:09:49 +0900

    バリデーション追加 : タイトルの重複

--- app/models/doing.rb
+++ app/models/doing.rb
@@ -5,5 +5,6 @@ class Doing < ApplicationRecord
 
   validates :title, presence: { message: "取り組み事項のタイトルは必須入力項目です" }
   validates :title, length: { maximum: 100, message: "タイトルは100文字以内にしてください" }
+  validates :title, uniqueness: { message: "既に使用されているタイトルは登録できません" }
   validates :summary, length: { maximum: 1000, message: "概要は1000文字以内にしてください" }
 end
