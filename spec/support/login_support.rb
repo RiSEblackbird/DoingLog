ad28ff7fc82cff8d7236779feea911be750d5767
SELA <RiSEblackbird@users.noreply.github.com>
2020-08-12 12:04:14 +0900

    ユーザー名を使用するログインは別途対応する

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -2,7 +2,7 @@ module LoginSupport
     def sign_in_as(user)
         visit root_path
         click_link "ログイン"
-        fill_in "ユーザー名", with: user.username
+        # fill_in "ユーザー名", with: user.username
         fill_in "Eメール", with: user.email
         fill_in "パスワード", with: user.password
         click_button "ログイン"
